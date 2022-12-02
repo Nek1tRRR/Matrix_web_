4e6343341d57827b24af7e36aed2a3b8c41ddeb3
Nek1tRRR <nekitrrr@yandex.ru>
2022-12-02 05:34:32 +0300

    aaaaa

--- app/tmpl/pages/profile/userPage.php
+++ app/tmpl/pages/profile/userPage.php
@@ -3,6 +3,7 @@
         <div class = 'profile-header'>
             <div class = 'user-avatar-container'>
                 <img src = '<?=$this -> user -> big_avatar();?>'>
+
             </div>
         </div>
     </div>
